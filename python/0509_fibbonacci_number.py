b860e6b54bc302c428bc26143490e0c9c9d735cc
Eric Lee <ericlovesmath@gmail.com>
2022-09-03 13:43:53 +0900

    509 Fibbonacci Numbers

---
+++ python/0509_fibbonacci_number.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        """
+        Link: https://leetcode.com/problems/fibonacci-number/
+        Return the nth fibbonacci number.
+        """
+
+        # Method 1: Dynamic Programming
+        # Bottom up, because recursion is slow
+        # We could also to recursion with caching too
+        # Space O(n), Time O(n)
+
+        fibb = [0, 1]
+        for _ in range(n - 1):
+            fibb.append(fibb[-2] + fibb[-1])
+        return fibb[n]
+
+        # Method 2: Binet's Formula
+        # Probably worse for interviews, but explicit formula exists
+        # Space O(1), Time O(1)
+
+        sq_5 = 5**0.5
+        return int(((1 + sq_5) ** n - (1 - sq_5) ** n) / (2**n * sq_5))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    assert sol.fib(0) == 0
+    assert sol.fib(1) == 1
+    assert sol.fib(2) == 1
+    assert sol.fib(3) == 2
+    assert sol.fib(4) == 3
+    assert sol.fib(5) == 5
